c7e65dedc5c8031a7fa6dcdd0280f00d13f2a5c1
PhDev <henriquedsg15@gmail.com>
2022-02-24 20:43:13 -0300

    commit1

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,21 @@
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class Main {
+
+	public static void main(String[] args) {
+		
+		Pessoa p1 = new Pessoa();
+		p1.setNome("Paulo");
+		p1.setDia("15");
+		p1.setMes("08");
+		p1.setAnoNasc(2005);
+		p1.setAltura(1.65);
+		
+		System.out.println(p1.imprimirDadosPessoa());
+		
+		System.out.println("A idade de " + p1.getNome() + " é: " + p1.somaIdade());
+
+	}
 
 }
--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,4 +1,62 @@
+import java.util.Calendar;
+import java.util.Date;
 
 public class Pessoa {
+	private String nome;
+	private String dia, mes;
+	private int anoNasc;
+	private double altura;
+	
+	private Date dataNascimento;
+	
+	public String getDia() {
+		return dia;
+	}
+	public void setDia(String dia) {
+		this.dia = dia;
+	}
+	public String getMes() {
+		return mes;
+	}
+	public void setMes(String mes) {
+		this.mes = mes;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getAnoNasc() {
+		return anoNasc;
+	}
+	public void setAnoNasc(int anoNasc) {
+		this.anoNasc = anoNasc;
+	}
+	public double getAltura() {
+		return altura;
+	}
+	public void setAltura(double altura) {
+		this.altura = altura;
+	}
+	
+	public String imprimirDadosPessoa() {
+	      String dados = "Nome: " + nome + "\n" + "Data de Nascimento: " + dia + 
+	    		         "/" + mes + "/" + anoNasc + "\n" + "Altura: " + altura;
+	return dados;}
+	
+	public int somaIdade() {
+		int anoNasc;
+		
+		Calendar dataAtual = Calendar.getInstance();
+		Integer mesAtual = dataAtual .get(Calendar.MONTH) + 1;
+		int validacaoData = Integer.parseInt(mes);
+		
+		if(validacaoData >= mesAtual) {
+			return anoNasc = (2022 - 1) - getAnoNasc();
+		}
+		
+		anoNasc = 2022 - getAnoNasc();
+	return anoNasc;}
 
 }
